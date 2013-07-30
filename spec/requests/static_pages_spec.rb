18b3a8020060b41764104f34329ade8a090941b3
Justin Leveck <jleveck@gmail.com>
2013-07-30 16:49:52 +0000

    Finish refactoring tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,51 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    include ApplicationHelper
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)     { 'Sample App' }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
     before  { visit help_path }
+    let(:heading)     { 'Help' }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before  { visit about_path }
+    let(:heading)     { 'About Us' }
 
-    it { should have_content('About Us') }
-    it { should have_title(full_title('About Us')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before  { visit contact_path }
+    let(:heading)     { 'Contact' }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    include ApplicationHelper
+    click_link "Help"
+    include ApplicationHelper
+    click_link "Contact"
+    include ApplicationHelper
+    click_link "Home"
+    click_link "Sign up now!"
+    include ApplicationHelper
+    click_link "sample app"
+    include ApplicationHelper
   end
 end
